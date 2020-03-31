334cc14858c7b848a014ba2e123816cde8563de4
rokib <akun.rokib.id@gmail.com>
2020-03-31 10:04:50 +0700

    publish file index

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var app = angular.module('myApp', []);
+app.controller('newsController', function($scope, $http){	
+    $http({method: 'POST', url: 'js/news.json'}).success(function(data){
+        $scope.beritas = data; // response data 
+    });
+ });
